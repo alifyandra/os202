8e6c377c81ad456fb5d5510178166b4b6965b73a
Ahmad Izzudin Alifyandra <alifyandra@yahoo.co.uk>
2020-09-25 23:35:53 +0700

    upgraded mkdirs.sh

--- experimental/w01/rmdirs.sh
+++ experimental/w01/rmdirs.sh
@@ -1,3 +1,7 @@
-for i in $(cat $1); do
-	rmdir $i
-done
+if [ -z $1 ];then
+	echo "Enter a txt file to extract words from"
+else
+	for i in $(cat $1); do
+		rmdir $i
+	done
+fi
---
+++ experimental/w01/test.py
@@ -0,0 +1,5 @@
+count = 0
+for i in range(5):
+	for j in range(i,5):
+		count +=1
+print(count)
