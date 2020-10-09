e49f722c17d56a6396e7e43bf0e1d940a61abf1b
alifyandra <alifyandra@yahoo.co.uk>
2020-10-09 17:11:20 +0700

    move script and update ranks and log

---
+++ SandBox/w02/logscript.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Script to automate logging process
+# Author: alifyandra
+
+printf "Please enter What week you wish to log.\n"
+printf "e.g. 'W01'/'W03'\n"
+
+read week
+
+printf "Would you like to see the logcodes list? (Y/N)\n"
+read list
+
+if [[ $list == "Y" || $list == "y" ]]; then
+	printf "\nFetching Log Code list from OS Website. \n"
+	sleep 1
+	printf "Please wait...\n"
+	curl https://osp4diss.vlsm.org/ETC/logCodes.txt || wget -qO - https://osp4diss.vlsm.org/ETC/logCodes.txt
+fi
+
+printf "Enter a logcode: \n"
+printf "e.g. 'L01'/'L20'\n"
+read code
+
+printf "How many minutes did this activity take?\n"
+printf "e.g."
+read time
+timelen=$(echo -n "$time" | wc -c)
+
+if [ $timelen -eq 1 ];then 
+	time="  $time"
+elif [ $timelen -eq 2 ];then
+	time=" $time"
+else 
+	time="$time"
+fi
+
+printf "Please enter a short description of it:\n"
+read desc
+
+printf "Would you like to add this to your log? (Y/N)\n"
+printf "ZCZC $week $time $code $desc\n"
+read add
+
+if [[ $add == "Y" || $add == "y" ]]; then
+	printf "ZCZC $week $time $code $desc\n" >> mylog.txt
+	printf "\nSuccessfully added!\n\n"
+	sleep 1
+	printf "Would you like to log again? (Y/N)\n"
+	read again
+	if [[ $again == "Y" || $again == "y" ]]; then
+		bash $0
+	fi
+	printf "Bye!"
+else
+	printf "Would you like to log again? (Y/N)\n"
+	read again
+	if [[ $again == "Y" || $again == "y" ]]; then
+		bash $0
+	fi
+	printf "Bye!"
+fi
+
+
--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,63 +1,23 @@
 #!/bin/bash
-# Script to automate logging process
-# Author: alifyandra
+# Alifyandra
+# PALSU BANGET SEKALI UBAH
 
-printf "Please enter What week you wish to log.\n"
-printf "e.g. 'W01'/'W03'\n"
+FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh myW03token.txt myW03disk.txt"
+SHA="SHA256SUM"
 
-read week
+rm -f $SHA $SHA.asc
 
-printf "Would you like to see the logcodes list? (Y/N)\n"
-read list
+echo "sha256sum $FILES > $SHA"
+sha256sum $FILES > $SHA
 
-if [[ $list == "Y" || $list == "y" ]]; then
-	printf "\nFetching Log Code list from OS Website. \n"
-	sleep 1
-	printf "Please wait...\n"
-	curl https://osp4diss.vlsm.org/ETC/logCodes.txt || wget -qO - https://osp4diss.vlsm.org/ETC/logCodes.txt
-fi
+echo "sha256sum -c $SHA"
+sha256sum -c $SHA
 
-printf "Enter a logcode: \n"
-printf "e.g. 'L01'/'L20'\n"
-read code
+echo "gpg -o $SHA.asc -a -sb $SHA"
+gpg -o $SHA.asc -a -sb $SHA
 
-printf "How many minutes did this activity take?\n"
-printf "e.g."
-read time
-timelen=$(echo -n "$time" | wc -c)
+echo "gpg --verify $SHA.asc $SHA"
+gpg --verify $SHA.asc $SHA
 
-if [ $timelen -eq 1 ];then 
-	time="  $time"
-elif [ $timelen -eq 2 ];then
-	time=" $time"
-else 
-	time="$time"
-fi
-
-printf "Please enter a short description of it:\n"
-read desc
-
-printf "Would you like to add this to your log? (Y/N)\n"
-printf "ZCZC $week $time $code $desc\n"
-read add
-
-if [[ $add == "Y" || $add == "y" ]]; then
-	printf "ZCZC $week $time $code $desc\n" >> mylog.txt
-	printf "\nSuccessfully added!\n\n"
-	sleep 1
-	printf "Would you like to log again? (Y/N)\n"
-	read again
-	if [[ $again == "Y" || $again == "y" ]]; then
-		bash $0
-	fi
-	printf "Bye!"
-else
-	printf "Would you like to log again? (Y/N)\n"
-	read again
-	if [[ $again == "Y" || $again == "y" ]]; then
-		bash $0
-	fi
-	printf "Bye!"
-fi
-
+exit 0
 
